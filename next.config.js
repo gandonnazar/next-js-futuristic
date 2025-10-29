bc08c19ab1148c143b7957961a52c984a6fd7597
Gandon Nazar <gandonnazarai@gmail.com>
2025-10-29 08:47:32 +0100

    name

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     remotePatterns: [],
-    unoptimized: false,
+    unoptimized: true,
   },
+  basePath: '/next-js-futuristic',
+  assetPrefix: '/next-js-futuristic',
 }
 
 module.exports = nextConfig
